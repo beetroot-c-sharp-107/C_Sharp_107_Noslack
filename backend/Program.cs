b8e99e49c8e18982ec60025cfc9fe983ed428d83
Alex Sabaka <alex.love.broadcast@gmail.com>
2023-11-29 20:42:58 +0200

    Added connection string to the PostgresSQL DB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ChatDbContext>((services, options) => {
+    var configuration = services.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetSection("ConnectionStrings:Postgres").Value;
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
