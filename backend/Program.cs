d1d6573644f98f08c95db771d7a04179d36b0687
YanaPrilipko <plilipkoyana@gmail.com>
2023-12-04 15:57:32 +0200

    Serilog

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,10 +3,17 @@ using backend;
 using backend.DTO.UserControllerDTO;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
