23a32fc9d436aa53e2aeff8e82ea0ffd840f630b
YanaPrilipko <143351384+YanaPrilipko@users.noreply.github.com>
2023-12-11 21:20:30 +0200

    Origin/features/serilog (#11)
    * Serilog
    * Now

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,10 +6,18 @@ using backend.DTO.ChatControllerDTO;
 using backend.DTO.UserControllerDTO;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
