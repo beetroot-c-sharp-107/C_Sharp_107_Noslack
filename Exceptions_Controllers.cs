a80ae966ff938c68481d98a455f23b83ca3b1e88
AlexxNikolaev <143645546+AlexxNikolaev@users.noreply.github.com>
2023-12-11 20:40:44 +0300

    Add files via upload

---
+++ Exceptions_Controllers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using backend.DTO.UserControllerDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace backend.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception thrown during request processing.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var errorResponse = new GenericErrorDTO
+            {
+                RequestID = context.TraceIdentifier,
+                ErrorMessage = "Internal Server Error",
+                ErrorType = "InternalServerError"
+            };
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var json = JsonConvert.SerializeObject(errorResponse);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
+
+
+
+
