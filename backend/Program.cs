e5b8726e8e0206c8c25cd798f32720da5a2c9b55
YanaPrilipko <plilipkoyana@gmail.com>
2023-12-11 20:45:30 +0200

    Now

--- backend/Program.cs
+++ backend/Program.cs
@@ -15,6 +15,7 @@ var logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
