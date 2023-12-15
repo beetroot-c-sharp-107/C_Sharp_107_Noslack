58761347f9bcb05a6a00a296e34607c5c7441921
Yurii-Shevchuk <yuriishvchk@gmail.com>
2023-12-15 15:33:35 +0200

    Added http context to services along with small typo fixes

--- backend/Hubs/ChatHub.cs
+++ backend/Hubs/ChatHub.cs
@@ -3,23 +3,26 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Hubs;
-{
     public class ChatHub : Hub
     {
-        public async Task SendMessage(int fromUserId, int toUserId, string message, ChatDbContext dbContext, CancellationToken cancellationToken = default)
+        public async Task SendMessage(int fromUserId, int toUserId, string message, HttpContextAccessor httpContextAccessor, ChatDbContext dbContext, CancellationToken cancellationToken = default)
         {
-            var chat = await dbContext.Chats.Where(x => (fromUserId == x.UserId1 || fromUserId == x.UserId2) && (toUserId == x.UserId1 || toUserId == x.UserId2)).FirstOrDefaultAsync();
-            //Not sure if it's the right way to get cookies from here
-            int.TryParse(Context.Features.Get<HttpContext>().Request.Cookies["currentUserId"], out int currentUserId);
+            var chat = await dbContext.Chats
+            .Where(x => (fromUserId == x.UserId1 || fromUserId == x.UserId2) && (toUserId == x.UserId1 || toUserId == x.UserId2))
+            .FirstOrDefaultAsync();
+
+            _ = int.TryParse(httpContextAccessor.HttpContext.Request.Cookies["currentUserId"], out int currentUserId);
+
             if(chat is null || currentUserId != fromUserId || currentUserId != toUserId )
             {
                 throw new Exception("The chat you're trying to access doesn't exist or current user is no a participant of the chat");
             }
+
             var messageEntity = new Message{ChatId = chat.Id, MessageText = message, UserId = currentUserId, SentDate = DateTime.UtcNow};
             await dbContext.Messages.AddAsync(messageEntity, cancellationToken);
             await dbContext.SaveChangesAsync(cancellationToken);
+            
             //We could also send some message to the sender probably but not sure if it's needed;
             await Clients.Caller.SendAsync("MessageSentSuccess");
         }
     }
-}
